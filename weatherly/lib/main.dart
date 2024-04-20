ae7ff81d5ad0982e4f3f8721f7e9cbb491fdc9f5
FMABrotherhoodX <31969916+FMABrotherhoodX@users.noreply.github.com>
2024-04-20 19:46:50 -0400

    Updating files
    deleted old set of files for homescreen app and login in favor of notification and weather api to get function first
    
    Co-Authored-By: jcaviness1 <109058376+jcaviness1@users.noreply.github.com>

--- weatherly/lib/main.dart
+++ weatherly/lib/main.dart
@@ -1,9 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'app.dart';
+import 'package:provider/provider.dart';
+import 'weather_api.dart';
+import 'notification_service.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(WeatherlyApp());
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weatherly',
+      home: WeatherScreen(),
+    );
+  }
+}
+
+class WeatherScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Weatherly')),
+      body: Center(
+        child: Text('Weather Information Displayed Here'),
+      ),
+    );
+  }
 }
